d5793b6c2b741676786b986689622e9e328e4040
skehdxhd96 <xman96@naver.com>
2023-05-10 10:45:39 +0900

    FEAT: 이벤트 api 테스트

--- src/test/java/com/example/timedeal/event/EventControllerTest.java
+++ src/test/java/com/example/timedeal/event/EventControllerTest.java
@@ -1,9 +1,8 @@
 package com.example.timedeal.event;
 
-import com.example.timedeal.Event.dto.EventAssembler;
-import com.example.timedeal.Event.dto.EventSaveRequest;
-import com.example.timedeal.Event.dto.EventSelectResponse;
+import com.example.timedeal.Event.dto.*;
 import com.example.timedeal.Event.entity.Event;
+import com.example.timedeal.Event.entity.PublishEvent;
 import com.example.timedeal.Event.service.EventService;
 import com.example.timedeal.common.dto.AuthUser;
 import com.example.timedeal.common.factory.EventFactory;
@@ -11,10 +10,7 @@ import com.example.timedeal.common.factory.UserFactory;
 import com.example.timedeal.user.entity.Administrator;
 import com.example.timedeal.user.service.LoginService;
 import com.example.timedeal.user.service.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,24 +22,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
-
 import static com.example.timedeal.ApiDocumentUtils.getDocumentRequest;
 import static com.example.timedeal.ApiDocumentUtils.getDocumentResponse;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
-import static org.assertj.core.api.InstanceOfAssertFactories.STRING;
+import static org.assertj.core.api.InstanceOfAssertFactories.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
@@ -63,13 +53,10 @@ public class EventControllerTest {
     public static final String USER_SESSION_KEY = "USER_ID";
     @Autowired
     private MockMvc mvc;
-
     @Autowired
     protected ObjectMapper objectMapper;
-
     @MockBean
     private UserService userService;
-
     @MockBean
     private LoginService loginService;
     @MockBean
@@ -175,27 +162,87 @@ public class EventControllerTest {
 
     @Test
     @DisplayName("이벤트 발행 테스트")
-    void event_register_test() {
+    void event_register_test() throws Exception {
+
+        Administrator administrator = UserFactory.administrator();
+        PublishEventSaveRequest publishEventSaveRequest = EventFactory.publishEventSaveRequest();
+        PublishEvent publishEvent = EventFactory.publishEvent();
 
         // given
+        given(loginService.getCurrentUser())
+                .willReturn(AuthUser.of(administrator));
+        given(EventAssembler.publishEvent(any(Event.class), any(PublishEventSaveRequest.class)))
+                .willReturn(publishEvent);
 
         // when
+        ResultActions perform = mvc.perform(post("/api/v1/event/publish")
+                .header("COOKIE", httpSession)
+                .content(objectMapper.writeValueAsString(publishEventSaveRequest))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE));
 
         // then
+        perform.andExpect(status().isNoContent());
+        verify(eventService, times(1))
+                .publish(any(PublishEventSaveRequest.class));
 
         // restdocs
+        perform.andDo(document("event-publish",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestHeaders(
+                        headerWithName(HttpHeaders.COOKIE).description("SESSION")
+                ),
+                requestFields(
+                        fieldWithPath("eventId").type(LONG).description("target event Id"),
+                        fieldWithPath("eventName").type(STRING).description("publish event name"),
+                        fieldWithPath("eventStartTime").type(LOCAL_DATE_TIME).description("publish event start time"),
+                        fieldWithPath("eventEndTime").type(LOCAL_DATE_TIME).description("publish event end time"),
+                        fieldWithPath("eventDesc").type(INTEGER).description("event's benefit. ex) 10 : 10% discount.")
+                )));
     }
 
     @Test
     @DisplayName("발행 이벤트 목록 조회 테스트")
-    void events_register_find_test() {
+    void events_register_find_test() throws Exception {
+
+        Administrator administrator = UserFactory.administrator();
+        List<PublishEventSelectResponse> publishEvents = EventFactory.publishevents().stream()
+                                                                                    .map(PublishEventSelectResponse::new)
+                                                                                    .collect(Collectors.toList());
+        String expectedResponse = objectMapper.writeValueAsString(publishEvents);
 
         // given
+        given(loginService.getCurrentUser())
+                .willReturn(AuthUser.of(administrator));
+        given(eventService.findPublishEventList())
+                .willReturn(publishEvents);
 
-        // when
+        // when, then
+        ResultActions perform = mvc.perform(get("/api/v1/event/publish")
+                        .header("COOKIE", httpSession)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(expectedResponse));
 
-        // then
+        verify(eventService, times(1))
+                .findPublishEventList();
 
         // restdocs
+        perform.andDo(document("find-publish-event-list",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestHeaders(
+                        headerWithName(HttpHeaders.COOKIE).description("SESSION")
+                ),
+                responseFields(
+                        fieldWithPath("[].id").type(ARRAY).description("publish event id"),
+                        fieldWithPath("[].eventName").type(ARRAY).description("publish event name"),
+                        fieldWithPath("[].eventStartTime").type(ARRAY).description("publish event start time"),
+                        fieldWithPath("[].eventEndTime").type(ARRAY).description("publish event end time"),
+                        fieldWithPath("[].eventDesc").type(ARRAY).description("event's benefit. ex) 10 : 10% discount")
+                )));
     }
 }
